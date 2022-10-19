ed83e2d3c5fa22086001b06e409c03fb346d013b
jpfsouza99 <jpfsouza99@gmail.com>
2022-10-18 22:16:22 -0300

    criando nav bar

--- lib/main.dart
+++ lib/main.dart
@@ -13,8 +13,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Black moon',
       theme: ThemeData(
+        scaffoldBackgroundColor: kBackGroundColor,
         primaryColor: KprimaryColor,
         textTheme: Theme.of(context).textTheme.apply(bodyColor: KTextColor),
         visualDensity: VisualDensity.adaptivePlatformDensity,
--- lib/screens/home/components/home_screen.dart
+++ lib/screens/home/components/home_screen.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(),
+    );
   }
 }
