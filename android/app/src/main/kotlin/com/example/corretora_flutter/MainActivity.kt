055d4c2aa0917cf92f478b10e5230b712c72762d
jpfsouza99 <jpfsouza99@gmail.com>
2022-10-18 21:53:55 -0300

    Iniciando projeto

---
+++ android/app/src/main/kotlin/com/example/corretora_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.corretora_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constantes.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const KprimaryColor = Color(0xFF0C9869);
+const KTextColor = Color(0xFF3C4046);
+const kBackGroundColor = Color(0xFFF9F8FD);
+
+const double kDefaultPadding = 10.0;
