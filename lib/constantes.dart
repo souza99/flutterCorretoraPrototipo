6db700ed2715c83c58e595f5f5c315ac1964353d
jpfsouza99 <jpfsouza99@gmail.com>
2022-10-18 22:21:15 -0300

    Buscando exemplo

--- lib/constantes.dart
+++ lib/constantes.dart
@@ -5,3 +5,7 @@ const KTextColor = Color(0xFF3C4046);
 const kBackGroundColor = Color(0xFFF9F8FD);
 
 const double kDefaultPadding = 10.0;
+
+
+//PESQUISAR EXEMPLOS
+//https://www.bing.com/videos/search?q=como+criar+um+app+em+flutter&&view=detail&mid=610E7999E43D4E75FD7F610E7999E43D4E75FD7F&&FORM=VRDGAR&ru=%2Fvideos%2Fsearch%3Fq%3Dcomo%2Bcriar%2Bum%2Bapp%2Bem%2Bflutter%26FORM%3DHDRSC3
